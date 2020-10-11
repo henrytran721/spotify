235973de96589b733bc2dc19d5f633a7d019f16b
henrytran721 <henryhtran07@gmail.com>
2020-10-10 17:06:35 -0700

    added top tracks page and pulled / displayed data

--- client/src/views/Spotify.jsx
+++ client/src/views/Spotify.jsx
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import '../sass/spotify.scss';
 
-export default class Spotify extends React.Component {
+function MenuBtn(props) {
+    const { access_token } = props;
+    return(
+        <div> 
+            <div className='menuBtnContainer'>
+                <a href={'/toptracks/?access_token=' + access_token}>Get My Top Tracks</a>
+                <a href={'/topartists/?access_token=' + access_token}>Get My Top Artists</a>
+                <a href={'/myplaylists/?access_token=' + access_token}>Get My Playlists</a>
+            </div>
+        </div>
+    )
+}
+
+export default class Spotify extends Component {
     constructor(props) {
         super(props);
         this.state = {
             url: '',
             loggedin: false,
             user: {},
-            userid: ''
+            userid: '',
+            access_token: ''
         }
     }
 
@@ -21,6 +36,7 @@ export default class Spotify extends React.Component {
         // retrieve our query parameter and isolate access token
         const queryString = new URLSearchParams(window.location.search);
         const access_token = queryString.get('access_token');
+        this.setState({access_token})
         // make sure our access token is valid and set state to true
         if(access_token !== undefined && window.location.href.indexOf('?access_token=')) {
             this.setState({loggedin: true});
@@ -30,25 +46,21 @@ export default class Spotify extends React.Component {
             .then((res) => res.json())
             .then(user => this.setState({user}))
             .catch(e => console.error(e))
-
-            fetch('https://api.spotify.com/v1/me/top/tracks', {
-                headers: {
-                    'Authorization': 'Bearer ' + access_token,
-                    "Accept": "application/json"
-                }
-            })
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data)
-            })
-            .catch((e) => {console.error(e)})
         }
     }
 
     render() {
+        const { loggedin, access_token } = this.state;
         return(
             <div>
-                <a href={this.state.url}>Log In with Spotify</a>
+                <div className='spotifyContainer'>
+                    <a className='spotifyBtn' href={this.state.url}>Log In with Spotify</a>
+                    {!loggedin ? <a className='spotifyBtn' href={this.state.url}>Log In with Spotify</a> : 
+                    <MenuBtn 
+                     access_token={access_token}
+                    />
+                    }
+                </div>  
             </div>
         )
     }
